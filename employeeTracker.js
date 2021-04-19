3bc0454cc7e5f14be578dd3e20cc6c911f8ef67b
Clare Henderson <henderson2350@gmail.com>
2021-04-19 19:42:20 -0400

    progress on functionality

--- employeeTracker.js
+++ employeeTracker.js
@@ -16,6 +16,11 @@ const connection = mysql.createConnection({
     database: 'employee_db',
   });
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  start();
+});
 
 function start() {
   inquirer
@@ -24,15 +29,42 @@ function start() {
         name: 'start',
         message: 'What would you like to do?',
         type: 'list',
-        choices: ['View all employees', 'View all employees by department', 'View all employees by Manager',
-      'Add employee', 'Remove employee', 'Update employee role', 'Update Employee Manager']
+        choices: [
+          'View all employees', 
+          'View all employees by department', 
+          'View all employees by Manager',
+          'Add employee', 
+          'Remove employee', 
+          'Update employee role', 
+          'Update Employee Manager', 
+          "Add role", 
+          "View all roles",
+          "View departments"
+        ]
       }
     ])
     .then((answer) => {
-      if (answer.start === "Add employee") {
-        addEmployee()
-      } else if (answer.start === "Remove employee") {
-        removeEmployee()
+      switch (answer.start) {
+        case 'Add employee':
+          addEmployee();
+          break;
+        
+        case 'Remove employee':
+          removeEmployee();
+          break;
+
+        case "View all roles":
+          viewRoles()
+          break;
+        
+        case 'View all employees':
+          viewAllEmployees()
+          break;
+
+        case "View departments":
+          viewDepartments()
+          break;
+
       }
     })
 }
@@ -41,12 +73,12 @@ function addEmployee() {
   inquirer
     .prompt([
       {
-        name: 'first-name',
+        name: 'first_name',
         message: 'What is the employee\'s first name?',
         type: 'input'
       },
       {
-        name: 'last-name',
+        name: 'last_name',
         message: 'What is the employee\'s last name?',
         type: 'input'
       },
@@ -54,34 +86,95 @@ function addEmployee() {
         name: 'role',
         message: 'What is the employee\'s role?',
         type: 'list',
-        choices: ["none yet"] //going too need to put all of the roles from the db in here as the choices
-
+        choices: ['Analyst','Accountant','Lawyer','Legal team aid','Marketer','Web Developer','Software Engineer','Salesperson'] //going too need to put all of the roles from the db in here as the choices
       },
-      {
-        name: 'manager',
-        message: 'Who is the employee\'s manager?',
-        type: 'list',
-        choices: ["None yet"] //need to put all of the managers from the db in here
-      }
       
     ])
-    // .then((answer) => {
-    //   connection.query(
-    //     'INSERT INTO employee SET ?'
-    //   )
-    // })
+    .then((answer) => {
+      var role_id = 0
+      switch (answer.role) {
+        case 'Analyst':
+          var role_id = 1;
+          break;
+        
+        case 'Accountant':
+          var role_id = 3;
+          break;
+
+        case 'Lawyer':
+          var role_id = 4;
+          break;
+        
+        case 'Legal team aid':
+          var role_id = 5;
+          break;
+        
+        case 'Marketer':
+          var role_id = 7;
+          break;
+        
+        case 'Web Developer':
+          var role_id = 8;
+          break;
+        
+        case 'Software Engineer':
+          var role_id = 2;
+          break;
+        
+        case 'Salesperson': 
+          var role_id = 6;
+          break;
+
+        default:
+          var role_id = 90
+      }
+      
+      values = [answer.first_name, answer.last_name, parseInt(role_id)]
+      console.log(values)
+      connection.query(
+        'INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)', (values), (err, res) => {
+          if (err) throw err
+          console.log('Employee added!')
+        }
+      )
+    })
 }
 
 function removeEmployee() {
-  inquirer
+  var nameList = []
+  connection.query(`SELECT first_name, last_name FROM employee`, (err, res) => {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      first = res[i].first_name;
+      last = res[i].last_name;
+      fullName = first + " " + last
+      nameList.push(fullName)
+    }
+    inquirer
     .prompt([
       {
         name: 'name',
         message: 'Who would you like to remove?',
         type: 'list',
-        choices: ["None yet"]
+        choices: nameList
       }
     ])
+    .then((response) => {
+      split = response.name.split(" ")
+      console.log(split)
+      for (var i = 0; i < res.length; i++) {
+        if (split[0] === res[i].first_name && split[1] === res[i].last_name) {
+          connection.query(`DELETE FROM employee WHERE first_name = ? AND last_name = ?`, 
+          [res[i].first_name, res[i].last_name], (err, res) => {
+            if (err) throw err
+          })
+        }
+      }
+      
+    })
+
+  })
+
 }
 
 function updateRole() {
@@ -99,8 +192,26 @@ function updateRole() {
       }
     ])
 }
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  start();
-});
+
+
+function viewAllEmployees() {
+  let query = 
+    'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id)'
+  connection.query(query, (err, res) => {
+    console.log(res)
+  })
+}
+
+function viewRoles() {
+  connection.query('SELECT * FROM role', (err, res) => {
+    if (err) throw err;
+    console.log(res)
+})
+}
+
+function viewDepartments() {
+  connection.query('SELECT * FROM role', (err, res) => {
+    if (err) throw err;
+    console.log(res)
+})
+}
--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -1,3 +1,61 @@
-USE employee_db
+USE employee_db;
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
+INSERT INTO department(name)
+VALUE ("Engineering");
+
+INSERT INTO department(name)
+VALUE ("Sales");
+
+INSERT INTO department(name)
+VALUE ("Legal");
+
+INSERT INTO department(name)
+VALUE ("Finance");
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Analyst", "100000", 4)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Software Engineer", "200000", 1)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Accountant", "300000", 4)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Lawyer", "250000", 3)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Legal team aid", "80000", 3)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Salesperson", "70000", 2)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Marketer", "60000", 2)
+
+INSERT INTO role(title, salary, department_id)
+VALUE ("Web Developer", "150000", 1);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Clare", "Henderson", 8);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Mims", "Reynolds", 1);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Caroline", "Huger", 7);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Avery", "Legg", 4);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Peyton", "LeCorgne", 5);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Sarah", "Lane", 3);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Halle", "Hughes", 2);
+
+INSERT INTO employee(first_name, last_name, role_id)
+VALUE ("Ella", "Sayre", 6);
